b5ae88e20a2ac005285336c8ccdb60b408a4608b
Pedro Oliveira <henlolpedro@gmail.com>
2023-01-16 10:31:16 -0300

    Initial commit

---
+++ foo/src/test/java/br/com/pedro/MainTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
